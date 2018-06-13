ec5d06458fc8fcb037376f295b2d146794de6b1b
Ruan Wei Kang <wei-kang.ruan@ubisoft.com>
2018-06-13 17:56:07 +0800

    Get owner name in PositionReporter

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "Gameframework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -18,8 +19,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-    UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on chair!"));
+    FString ObjectName = GetOwner()->GetName();
+    UE_LOG(LogTemp, Warning, TEXT("Position reporter reporting for duty on %s!"), *ObjectName);
 }
 
 
